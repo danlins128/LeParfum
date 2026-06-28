9d65fba2cda3777b587cb06b8af2a72f0885f003
danlins128 <danlins8049@gmail.com>
2026-06-27 23:40:35 -0300

    Mapping and Context Set

---
+++ LeParfum.Data/Context/LeParfumDbContext.cs
@@ -0,0 +1,35 @@
+using LeParfum.Data.Entities;
+using LeParfum.Data.Mappings;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeParfum.Data.Context
+{
+    public class LeParfumDbContext : DbContext
+    {
+        public LeParfumDbContext(DbContextOptions<LeParfumDbContext> options) :base(options) {}
+
+        public DbSet<BrandEntity> Brands { get; set; }
+        public DbSet<GenderEntity> Genders { get; set; }
+        public DbSet<ProductEntity> Products { get; set; }
+        public DbSet<StockEntity> Stocks { get; set; }
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new BrandEntityMapping());
+            
+            modelBuilder.ApplyConfiguration(new GenderEntityMapping());
+            
+            modelBuilder.ApplyConfiguration(new ProductEntityMapping());
+            
+            modelBuilder.ApplyConfiguration(new StockEntityMapping());
+            
+            modelBuilder.ApplyConfiguration(new UserEntityMapping());
+
+            base.OnModelCreating(modelBuilder);
+
+            
+
+        }
+    }
+}
